fc20b047d99b56db3a016072a56ed0ec23d936e5
quarentyne <peresada1997@gmail.com>
2022-01-10 15:32:02 +0200

    The generator was created

---
+++ Password Generatot/Password-Generator.js
@@ -0,0 +1,43 @@
+'use strict'
+Array.prototype.shuffle = function () {
+    for (let i = this.length - 1; i > 0; i--) {
+        let num = Math.floor(Math.random() * (i + 1));
+        let d = this[num];
+        this[num] = this[i];
+        this[i] = d;
+    }
+    return this;
+}
+
+function generatePassword(length) {
+    const abc = "abcdefghijklmnopqrstuvwxyz";
+    const ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '0123456789!@#$%^&*()-=|/_<>,.`~';
+    let pass = [];
+    let nABC = generateRandomNumber(1, length - 2);
+    length = length - nABC;
+    let nSymbols = generateRandomNumber(1, length - 2);
+    length = length - nSymbols;
+    for (let i = 0; i <= nABC; i++) {
+        pass.push(ABC[generateRandomNumber(0, ABC.length - 1)]);
+    }
+    for (let i = 0; i <= nSymbols; i++) {
+        pass.push(symbols[generateRandomNumber(0, symbols.length - 1)]);
+    }
+    for (let i = 0; i <= length; i++) {
+        pass.push(abc[generateRandomNumber(0, abc.length - 1)]);
+    }
+    pass = pass.shuffle();
+    return pass.join('');
+}
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function generateLength() {
+    //в мин и макс можно будет попробовать передавать значения со страницы сайта
+    let min = 8;
+    let max = 15;
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
